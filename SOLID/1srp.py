67f20d1896c11b340c61eb8b80c89bf78a603103
SwapnilP <ddevil691@gmail.com>
2023-08-29 06:45:45 -0400

    first commit

---
+++ SOLID/1srp.py
@@ -0,0 +1,52 @@
+class Journal:
+    def __init__(self):
+        self.entries = []
+        self.count = 0
+
+    def add_entry(self, text):
+        self.entries.append(f"{self.count}: {text}")
+        self.count += 1
+
+    def remove_entry(self, pos):
+        del self.entries[pos]
+
+    def __str__(self):
+        return "\n".join(self.entries)
+
+    # breaks SRP
+    # save/load responsibility + Journal
+    def save(self, filename):
+        file = open(filename, "w")
+        file.write(str(self))
+        file.close()
+
+    def load(self, filename):
+        pass
+
+    def load_from_web(self, uri):
+        pass
+
+
+# All load and save methods could be grouped in separate class
+
+class PersistenceManager:
+
+    @staticmethod
+    def save_to_file(journal, filename):
+        file = open(filename, "w")
+        file.write(str(journal))
+        file.close()
+
+
+j = Journal()
+j.add_entry("I cried today.")
+j.add_entry("I ate a bug.")
+print(f"Journal entries:\n{j}\n")
+
+p = PersistenceManager()
+file = r'c:\temp\journal.txt'
+p.save_to_file(j, file)
+
+# verify!
+with open(file) as fh:
+    print(fh.read())
